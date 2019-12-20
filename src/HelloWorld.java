189b9e92c5b481209c9fd7677933c7ef44543df0
shenxwan <shenxwan@cisco.com>
2019-12-20 17:49:36 +0800

    add test file and add more function in VisitBinaryTree

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,14 +4,6 @@ import Utils.VisitBinaryTree;
 // This file is to test code
 public class HelloWorld {
     public static void main(String[] args) {
-        TreeNode tree = new TreeNode(1);
-        tree.left = new TreeNode(2);
-        tree.right = new TreeNode(3);
-        tree.left.left = new TreeNode(4);
-        tree.left.right = new TreeNode(5);
 
-//        VisitBinaryTree.visitWithStack(tree);
-        VisitBinaryTree.visitWithRecursionPreorder(tree);
-//        VisitBinaryTree.morrisPreorderTraverse(tree);
     }
 }
--- src/Utils/VisitBinaryTree.java
+++ src/Utils/VisitBinaryTree.java
@@ -1,15 +1,16 @@
 package Utils;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Stack;
 
 public class VisitBinaryTree {
-    public static void visitWithStack(TreeNode root) {
+    public static void visitWithStackPreorder(TreeNode root) {
         Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
         while (!stack.empty()) {
             TreeNode tmp = stack.pop();
             System.out.println(tmp.val);
-            //Need to reverse it because stack is First In Last Our type.
             if (tmp.right != null) {
                 stack.push(tmp.right);
             }
@@ -20,11 +21,65 @@ public class VisitBinaryTree {
         }
     }
 
+    public static void visitWithStackInorder(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        TreeNode tmp = stack.pop();
+        while (!stack.empty() || tmp != null) {
+            while (tmp != null) {
+                stack.push(tmp);
+                tmp = tmp.left;
+            }
+            tmp = stack.pop();
+            System.out.println(tmp.val);
+            tmp = tmp.right;
+        }
+    }
+
+    public static void visitTreeWithStackPostorder(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur;
+        cur = root;
+        while (cur != null || !stack.empty()) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                TreeNode tmp = stack.peek().right;
+                if (tmp == null) {
+                    tmp = stack.pop();
+                    System.out.println(tmp.val);
+                    while (!stack.empty() && tmp == stack.peek().right) {
+                        tmp = stack.pop();
+                        System.out.println(tmp.val);
+                    }
+                } else {
+                    cur = tmp;
+                }
+            }
+        }
+    }
+
+
     public static void visitWithRecursionPreorder(TreeNode root) {
         if (root == null) return;
         System.out.println(root.val);
         visitWithRecursionPreorder(root.left);
         visitWithRecursionPreorder(root.right);
+    }
+
+    public static void visitWithRecursionInorder(TreeNode root) {
+        if (root == null) return;
+        visitWithRecursionInorder(root.left);
+        System.out.println(root.val);
+        visitWithRecursionInorder(root.right);
+    }
+
+    public static void visitWithRecursionPostorder(TreeNode root) {
+        if (root == null) return;
+        visitWithRecursionPostorder(root.left);
+        visitWithRecursionPostorder(root.right);
+        System.out.println(root.val);
     }
 
     public static void morrisPreorderTraverse(TreeNode root) {
@@ -49,5 +104,31 @@ public class VisitBinaryTree {
                 }
             }
         }
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur;
+        cur = root;
+        while (cur != null || !stack.empty()) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                TreeNode tmp = stack.peek().right;
+                if (tmp == null) {
+                    tmp = stack.pop();
+                    list.add(tmp.val);
+                    while (!stack.empty() && tmp == stack.peek().right) {
+                        tmp = stack.pop();
+                        list.add(tmp.val);
+                    }
+                } else {
+                    cur = tmp;
+                }
+            }
+        }
+        return list;
     }
 }
